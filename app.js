fd56d539d2dfe6e4cd46ca64877922af67779386
Brian Mitchell <bman4789@gmail.com>
2018-01-25 22:33:29 -0600

    Update deps, use airbnb preset for eslint, more es2015+ conventions

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-let express = require('express');
-let path = require('path');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
-let fs = require('fs');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-let routes = require('./routes/index');
+const routes = require('./routes/index');
 
-let app = express();
+const app = express();
 
-app.locals.stats = {'bandwidth': 0, 'startup': new Date()};
+app.locals.stats = { bandwidth: 0, startup: new Date() };
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get('/files/:file', function(req, res, next) {
-    if (req.params.file in app.locals.stats) {
-        app.locals.stats[req.params.file] += 1;
-    } else {
-        app.locals.stats[req.params.file] = 1;
-    }
-    app.locals.stats['bandwidth'] += fs.statSync(path.join(__dirname, 'public/files/'+req.params.file))['size'];
-    console.log(app.locals.stats);
-    next();
+app.get('/files/:file', (req, res, next) => {
+  if (req.params.file in app.locals.stats) {
+    app.locals.stats[req.params.file] += 1;
+  } else {
+    app.locals.stats[req.params.file] = 1;
+  }
+  app.locals.stats.bandwidth += fs.statSync(path.join(__dirname, `public/files/${req.params.file}`)).size;
+  console.log(app.locals.stats);
+  next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -48,23 +48,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,43 @@
-let express = require('express');
-let router = express.Router();
-let fs = require('fs');
-let path = require('path');
-let filesize = require('file-size');
+const express = require('express');
+
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const filesize = require('file-size');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    let p = 'public/files';
-    let jsons = [];
-    fs.readdir(p, function(err, files) {
-        if (err) throw err;
+router.get('/', (req, res) => {
+  const p = 'public/files';
+  const jsons = [];
+  fs.readdir(
+    p, (err, files) => {
+      if (err) throw err;
 
-        files.map(function(file) {
-            return path.join(p, file);
-        }).filter(function(file) {
-            return fs.statSync(file).isFile();
-        }).forEach(function(file) {
-            if (path.extname(file) === '.json') {
-                fs.readFile(file, 'utf8', function(err, data) {
-                    if (err) throw err;
-                    let tempFile = JSON.parse(data);
-                    let sizeInBytes = fs.statSync(p+'/'+tempFile['filename'])['size'];
-                    tempFile['filesize'] = filesize(sizeInBytes).human('si');
-                    if (tempFile['filename'] in req.app.locals.stats) {
-                        tempFile['downloads'] = req.app.locals.stats[tempFile['filename']];
-                    } else {
-                        tempFile['downloads'] = 0;
-                    }
-                    jsons.push(tempFile);
-                });
-                console.log('Found %s', file);
-            }
+      files
+        .map(file => path.join(p, file))
+        .filter(file => fs.statSync(file).isFile() && path.extname(file) === '.json')
+        .forEach((file) => {
+          fs.readFile(file, 'utf8', (e, data) => {
+            if (e) throw e;
+            const tempFile = JSON.parse(data);
+            const sizeInBytes = fs.statSync(`${p}/${tempFile.filename}`).size;
+            tempFile.filesize = filesize(sizeInBytes).human('si');
+            tempFile.downloads = tempFile.filename in req.app.locals.stats
+              ? req.app.locals.stats[tempFile.filename]
+              : 0;
+            jsons.push(tempFile);
+          });
+          console.log('Found %s', file);
         });
     },
     res.render('index', {
-        title: 'LAN Party Resources',
-        subtitle: 'Games and other resources to alleviate our time distributing files.',
-        description: 'Each file <em>should</em> contain instructions on how to install.',
-        bandwidth: 'We have served ' + filesize(req.app.locals.stats['bandwidth']).human('si') + ' since ' + req.app.locals.stats['startup'].toLocaleString(),
-        files: jsons.sort(function(a, b) {
-            return a.title.localeCompare(b.title);
-        })
-    })
-    );
+      title: 'LAN Party Resources',
+      subtitle: 'Games and other resources to alleviate our time distributing files.',
+      description: 'Each file <em>should</em> contain instructions on how to install.',
+      bandwidth: `We have served ${filesize(req.app.locals.stats.bandwidth).human('si')} since ${req.app.locals.stats.startup.toLocaleString()}`,
+      files: jsons.sort((a, b) => a.title.localeCompare(b.title)),
+    }),
+  );
 });
 
 module.exports = router;
